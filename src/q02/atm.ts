e16d42c45840220cbad972d84e6ff1a35946ab30
Sorawit Wongthim <68011120@kmitl.ac.th>
2025-12-15 09:38:06 +0700

    q2q3

---
+++ src/q02/atm.ts
@@ -0,0 +1,12 @@
+const balance = Number(process.argv[2]);
+const amount = Number(process.argv[3]);
+if(Number.isNaN(balance)||Number.isNaN(amount)){
+    console.log(`Invalid Input`);
+}
+if(amount>balance){
+    console.log(`Insufficient balance`);
+}else if(amount>5000){
+    console.log(`Exceeds per-withdrawal limit`);
+}else{
+    console.log(`Withdrawal approved`);
+}
