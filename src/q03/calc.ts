f21b005db409dff3f7225051a1c5201852ae0e6f
bsrxwt <sorawit281249@gmail.com>
2025-12-21 14:56:52 +0700

    calstest03

--- src/q03/calc.ts
+++ src/q03/calc.ts
@@ -1,29 +1,31 @@
 const op = process.argv[2];
 const Num1 = process.argv[3];
 const Num2 = process.argv[4];
-if(op===undefined||Num1===undefined||Num2===undefined||op===''||Num1===''||Num2===''){
+if(op===undefined||Num1===undefined||Num2===undefined||Num1.trim()===''||Num2.trim()===''){
   console.log(`Invalid input`);
+  process.exit();
+}
+if (op.trim() === "") {
+  console.log("Invalid operator");
   process.exit();
 }
 const n1 = Number(Num1);
 const n2 = Number(Num2);
-let result = 0;
 if(Number.isNaN(n1)||Number.isNaN(n2)){
   console.log(`Invalid input`);
   process.exit();
 }else{
 const oplw = op.toLowerCase();
 switch(oplw){
-  case "add": result = n1+n2; result.toString(); console.log(result);  break;
-  case "sub": result = n1-n2; result.toString(); console.log(result); break;
-  case "mul": result = n1*n2; result.toString(); console.log(result); break;
+  case "add": console.log(String(n1+n2));  break;
+  case "sub": console.log(String(n1-n2)); break;
+  case "mul": console.log(String(n1*n2)); break;
   case "div": 
   if(n2==0){
+    console.log("Invalid input");
     process.exit();
   }else{
-    result = n1/n2;
-    result.toString();
-    console.log(result);
+    console.log(String(n1/n2));
   }
   break;
   default: console.log(`Invalid operator`);
