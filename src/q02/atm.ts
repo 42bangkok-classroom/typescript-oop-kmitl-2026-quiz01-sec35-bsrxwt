1776cbaf1ff3cf8f0c53fbff49266fa628339077
bsrxwt <sorawit281249@gmail.com>
2025-12-21 14:05:21 +0700

    atmtest01

--- src/q02/atm.ts
+++ src/q02/atm.ts
@@ -1,12 +1,12 @@
 const balance = process.argv[2];
 const amount = process.argv[3];
 let ba = Number(balance);
-let am = Number(balance);
+let am = Number(amount);
 if(balance===''||amount===''){
     console.log(`Invalid Input`);
     process.exit();
 }
-else if(Number.isNaN(ba)||Number.isNaN(am)){
+if(Number.isNaN(ba)||Number.isNaN(am)){
     console.log(`Invalid Input`);
     process.exit();
 }
