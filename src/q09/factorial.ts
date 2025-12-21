537bec883e8a770efedd52d2a7df2ec020008daf
bsrxwt <sorawit281249@gmail.com>
2025-12-21 17:08:14 +0700

    fac01

--- src/q09/factorial.ts
+++ src/q09/factorial.ts
@@ -1,14 +1,19 @@
-const number = Number(process.argv[2]);
-if(Number.isNaN(number)||number<0||number%1!==0){
+const number = process.argv[2];
+if(number===undefined||number.trim()===""){
     console.log(`Invalid Input`);
     process.exit();
 }
-if(number===0){
+let num = Number(number);   
+if(Number.isNaN(num)||num<0||num%1!==0){
+    console.log(`Invalid Input`);
+    process.exit();
+}
+if(num===0){
     console.log(`1`);
     process.exit();
 }
 let result = 1;
-for(let i=number;i>=1;i--){
+for(let i=num;i>=1;i--){
     result *= i;
 }
-console.log(result);
+console.log(String(result));
