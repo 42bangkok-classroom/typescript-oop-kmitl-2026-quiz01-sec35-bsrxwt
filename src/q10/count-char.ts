367f4e06cddcb987dc22d33261df5a0e2ddd14c6
bsrxwt <sorawit281249@gmail.com>
2025-12-21 17:14:48 +0700

    c

---
+++ src/q10/count-char.ts
@@ -0,0 +1,20 @@
+const str = process.argv[2];
+if(!str||str.length===0){
+    console.log(`Invalid Input`);
+    process.exit();
+}
+let ltcounted:number = 0;
+let dgcounted:number = 0;
+let otcounted:number = 0;
+for(let i=0;i<str.length;i++){
+    if((str[i]>='a'&&str[i]<='z')||(str[i]>='A'&&str[i]<='Z')){
+        ltcounted++;
+    }else if(str[i]>='0'&&str[i]<='9'){
+        dgcounted++;
+    }else{
+        otcounted++;
+    }   
+}
+console.log(`Letters: ${ltcounted}`);
+console.log(`Digits: ${dgcounted}`);
+console.log(`Others: ${otcounted}`);
