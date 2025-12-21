0c1897b733c2192ba3f331ec09c499e8e720a897
bsrxwt <sorawit281249@gmail.com>
2025-12-21 14:14:20 +0700

    calstest01

--- src/q03/calc.ts
+++ src/q03/calc.ts
@@ -1,7 +1,7 @@
 const op = process.argv[2];
 const Num1 = Number(process.argv[3]);
 const Num2 = Number(process.argv[4]);
-if(!op||Number.isNaN(Num1)||Number.isNaN(Num2)){
+if(op===''||Number.isNaN(Num1)||Number.isNaN(Num2)){
   console.log(`Invalid input`);
   process.exit();
 }else{
